20455159a346d075acaa08849e08a8773325a2a4
Shohei Aoki <shoaok@gmail.com>
2017-04-03 23:51:48 +0900

    add sigmoid

--- src/deepclj/core.clj
+++ src/deepclj/core.clj
@@ -1,7 +1,6 @@
 (ns deepclj.core
   (:gen-class)
-  (:use incanter.core))
-
+  (:use (incanter core stats charts io))) 
 (defn AND [x1 x2]
   (let [x (matrix [x1 x2]) 
         w (matrix [0.5 0.5])
@@ -29,7 +28,31 @@
     0
     1)))
 
+(defn XOR [x1 x2]
+  (let [s1 (NAND x1 x2)
+        s2 (OR x1 x2)
+        y (AND s1 s2)]
+    y))
+
+(defn step_function [x]
+  (if (> x 0)
+    1
+    0))
+
+(defn test_step_function []
+  (let [x (map #(+ -5 (* (bigdec 0.1) %1)) (range 100))]
+    (map step_function x)))
+
+(defn sigmoid [x]
+  (/ 1 (+ 1 (exp (* -1 x)))))
+
+(defn test_sigmoid []
+  (let [x (map #(+ -5 (* (bigdec 0.1) %1)) (range 100))]
+    (map sigmoid x)))
+
+(defn test_view_sigmoid []
+  (view (function-plot sigmoid -5 5)))
+
+
 (defn -main [] 
-  (println (AND 1 1))
-  (println (NAND 1 0))
-  (println (OR 1 0)))
+  (println (test_view_sigmoid)))
